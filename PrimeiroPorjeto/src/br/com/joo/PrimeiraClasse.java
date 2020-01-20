595879f9b9b654a177e6ddbedf9a593b0def2f81
menezescarlos <menezesoliveira40@hotmail.com>
2020-01-20 14:42:10 -0200

    Java é Vida

--- PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
+++ PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
@@ -4,5 +4,6 @@ public class PrimeiraClasse {
  public static void main(String[] args) {
 	System.out.println("Olá mundo");
 	System.out.println("Olá Brasil");
+	System.out.println("Java é Vida");
 }
 }

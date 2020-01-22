22821512f315664bbdef8cdf25d2c2dd9648aab0
menezescarlos <menezesoliveira40@hotmail.com>
2020-01-22 10:17:29 -0200

    Estudo Pessoal

--- PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
+++ PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
@@ -4,5 +4,6 @@ public class PrimeiraClasse {
  public static void main(String[] args) {
 	System.out.println("Primeiro Projeto");
 	System.out.println("Olá mundo");
+	System.out.println("Estudo Pessoal");
 }
 }

aba6afbf78ad4445b96f80f4456d0835b0232980
menezescarlos <menezesoliveira40@hotmail.com>
2020-01-22 10:07:46 -0200

    Olá mundo atualizado

--- PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
+++ PrimeiroPorjeto/src/br/com/joo/PrimeiraClasse.java
@@ -3,8 +3,5 @@ package br.com.joo;
 public class PrimeiraClasse {
  public static void main(String[] args) {
 	System.out.println("Olá mundo");
-	System.out.println("Olá Brasil");
-	System.out.println("Java é Vida");
-	System.out.println("Java");
 }
 }

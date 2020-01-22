77bf878e5286f4e258962dc0c98d114c934bc37d
menezescarlos <menezesoliveira40@hotmail.com>
2020-01-22 12:06:34 -0200

    Variaveis versao 1.0

---
+++ PrimeiroPorjeto/src/br/com/joo/variaveis/Variaveis.java
@@ -0,0 +1,19 @@
+package br.com.joo.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		/*
+		 * Declaração, atribuição de valores, casting e comparação de variáveis
+		 * Controle de fluxo através de if e else
+		 * instruções de laço For e while, controle de fluxo com break e continue
+		 */
+		
+		//Declaração
+		//tipo  + Nome da variavel
+		int x = 50;
+		System.out.println(x);
+
+	}
+
+}
